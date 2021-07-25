4e477e64e2a0c86e91ce3164cd0217a46ea7e4e4
Bogdan107 <bogdan.pylypenko107@gmail.com>
2021-07-25 11:43:00 +0300

    add hook to restore_spoofed_macs()

--- spoof_mac_automatically.sh
+++ spoof_mac_automatically.sh
@@ -2,7 +2,7 @@
 
 # spoof_mac_automatically airgeddon plugin
 
-# Version:    0.2
+# Version:    0.2.1
 # Author:     Bogdan107
 # Repository: https://github.com/Bogdan107/airgeddon-plugins
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -47,6 +47,12 @@ plugin_minimum_ag_affected_version="10.0"
 plugin_maximum_ag_affected_version=""
 
 plugin_distros_supported=("*")
+
+# Spoof MAC-address after each interface selection action.
+function spoof_mac_automatically_posthook_restore_spoofed_macs() {
+    debug_print
+    spoofed_mac=0
+}
 
 # Spoof MAC-address after each interface selection action.
 function spoof_mac_automatically_posthook_select_interface() {
