fcb798ec24bc4f11985940161cd1bb4d2cfc8126
Bogdan107 <bogdan.pylypenko107@gmail.com>
2021-07-27 11:29:46 +0300

    uplevel minimum airgeddon version to 10.42

--- spoof_mac_automatically.sh
+++ spoof_mac_automatically.sh
@@ -2,7 +2,7 @@
 
 # spoof_mac_automatically airgeddon plugin
 
-# Version:    0.2.4
+# Version:    0.2.5
 # Author:     Bogdan107
 # Repository: https://github.com/Bogdan107/airgeddon-plugins
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -42,7 +42,7 @@ function spoof_mac_automatically() {
 
 ###### PLUGIN REQUIREMENTS ######
 
-plugin_minimum_ag_affected_version="10.0"
+plugin_minimum_ag_affected_version="10.42"
 plugin_maximum_ag_affected_version=""
 
 plugin_distros_supported=("*")
